88b2983157a07262a32f40769359d225a3054966
sanu209 <shanverma2221@gmail.com>
2026-04-28 12:45:16 +0530

    Create stack.py

---
+++ stack.py
@@ -0,0 +1,16 @@
+# Stack using push & pop
+stack = []
+
+# Push elements
+stack.append(10)
+stack.append(20)
+stack.append(30)
+
+print("Stack after push:", stack)
+
+# Pop element
+popped = stack.pop()
+print("Popped element:", popped)
+
+print("Stack after pop:", stack)
+
