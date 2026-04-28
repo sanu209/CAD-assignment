71a36dafa37f4535fce9a4ca37727c8ded1d69a8
sanu209 <shanverma2221@gmail.com>
2026-04-28 12:45:53 +0530

    Create queue.py

---
+++ queue.py
@@ -0,0 +1,14 @@
+queue = []
+
+# Enqueue elements
+queue.append(10)
+queue.append(20)
+queue.append(30)
+
+print("Queue after enqueue:", queue)
+
+# Dequeue element
+removed = queue.pop(0)
+print("Removed element:", removed)
+
+print("Queue after dequeue:", queue)
